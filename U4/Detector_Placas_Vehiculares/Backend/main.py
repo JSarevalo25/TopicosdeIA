ab69565e7e0e4daf632256b9f91c23f620e685f8
Jose Antonio Sanchez Arevalo <124337197+JSarevalo25@users.noreply.github.com>
2025-11-30 20:31:26 -0700

    Update main.py

--- U4/Detector_Placas_Vehiculares/Backend/main.py
+++ U4/Detector_Placas_Vehiculares/Backend/main.py
@@ -6,7 +6,7 @@ from app.api.routes import router as placas_router
 from app.api.correo_routes import router as correo_router
 from app.db.database import engine, Base
 
-
+# Preparamos el fastapi
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     db_path = Path(__file__).resolve().parent / "db" / "placas.db"
@@ -15,10 +15,11 @@ async def lifespan(app: FastAPI):
             await conn.run_sync(Base.metadata.create_all)
     yield
 
-
+# Obtenemos la variable 
 app = FastAPI(lifespan=lifespan)
-
+# agregamos las apis de placas y correo para mostrarlo en Swagger
 app.include_router(placas_router, prefix="/placas", tags=["placas"])
 
 app.include_router(correo_router, prefix="/correo", tags=["correo"])
+
 
